1a50886fd9cdf3d4aa92647f580890b4eefdf7c1
Vaibhav Nigam <vaibnigam404@gmail.com>
2026-05-22 01:34:45 +0530

    Swagger for order service

--- order-service/src/main/java/com/example/orderservice/controller/OrderController.java
+++ order-service/src/main/java/com/example/orderservice/controller/OrderController.java
@@ -4,11 +4,15 @@ import com.example.orderservice.dto.OrderRequestDTO;
 import com.example.orderservice.dto.OrderResponseDTO;
 import com.example.orderservice.entity.Order;
 import com.example.orderservice.service.OrderService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+
 import jakarta.validation.Valid;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+@Tag(name = "Order APIs", description = "Operations related to Orders")
 @RestController
 @RequestMapping("/orders")
 public class OrderController {
@@ -19,11 +23,19 @@ public class OrderController {
         this.orderService = orderService;
     }
 
+    @Operation(
+            summary = "Create Order",
+            description = "Creates a new order after customer validation and payment processing"
+    )
     @PostMapping
     public OrderResponseDTO createOrder(@Valid @RequestBody OrderRequestDTO requestDTO) {
         return orderService.createOrder(requestDTO);
     }
 
+    @Operation(
+            summary = "Get All Orders",
+            description = "Fetches all orders from database"
+    )
     @GetMapping
     public List<Order> getAllOrders() {
         return orderService.getAllOrders();
